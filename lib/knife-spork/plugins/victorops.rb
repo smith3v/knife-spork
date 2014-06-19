89128e1da9203d2aece252fd28fbc64ad508d540
Vyacheslav Kuznetsov <kuznetsovvv@gmail.com>
2014-06-19 22:48:07 +0400

    Added other messages for VictorOps chat.

--- lib/knife-spork/plugins/victorops.rb
+++ lib/knife-spork/plugins/victorops.rb
@@ -15,6 +15,98 @@ module KnifeSpork
         end
       end
 
+      def after_bump
+        send_message('knife spork bump', "#{organization}#{current_user} bumped from #{cookbooks.collect{ |c| "  #{c.name}@#{c.version}" }.join(" ")}")
+      end
+
+      def after_upload
+        send_message('knife spork upload', "#{organization}#{current_user} uploaded the following cookbooks:\n#{cookbooks.collect{ |c| "  #{c.name}@#{c.version}" }.join("\n")}")
+      end
+
+      def after_promote_remote
+        send_message('knife spork promote remote', "#{organization}#{current_user} promoted the following cookbooks:\n#{cookbooks.collect{ |c| "  #{c.name}@#{c.version}" }.join("\n")} to #{environments.collect{ |e| "#{e.name}" }.join(", ")}")
+      end
+
+      def after_environmentfromfile
+        send_message('knife spork environment from file', "#{organization}#{current_user} uploaded environment #{object_name}")
+      end
+
+      def after_environmentedit
+        send_message('knife spork environment edit', "#{organization}#{current_user} edited environment #{object_name}")
+      end
+
+      def after_environmentcreate
+        send_message('knife spork environment create', "#{organization}#{current_user} created environment #{object_name}")
+      end
+
+      def after_environmentdelete
+        send_message('knife spork environment delete', "#{organization}#{current_user} deleted environment #{object_name}")
+      end
+
+      def after_rolefromfile
+        send_message('knife spork role from file', "#{organization}#{current_user} uploaded role #{object_name}")
+      end
+
+      def after_roleedit
+        send_message('knife spork role edit', "#{organization}#{current_user} edited role #{object_name}")
+      end
+
+      def after_rolecreate
+        send_message('knife spork role create', "#{organization}#{current_user} created role #{object_name}")
+      end
+
+      def after_roledelete
+        send_message('knife spork role delete', "#{organization}#{current_user} deleted role #{object_name}")
+      end
+
+      def after_databagedit
+        send_message('knife spork data bag edit', "#{organization}#{current_user} edited data bag item #{object_name}:#{object_secondary_name}")
+      end
+
+      def after_databagcreate
+        send_message('knife spork data bag create', "#{organization}#{current_user} created data bag #{object_name}")
+      end
+
+      def after_databagdelete
+        send_message('knife spork data bag delete', "#{organization}#{current_user} deleted data bag #{object_name}")
+      end
+
+      def after_databagitemdelete
+        send_message('knife spork data bag item delete', "#{organization}#{current_user} deleted data bag item #{object_name}:#{object_secondary_name}")
+      end
+
+      def after_databagfromfile
+        send_message('knife spork data bag from file', "#{organization}#{current_user} uploaded data bag item #{object_name}:#{object_secondary_name}")
+      end
+
+      def after_nodeedit
+        send_message('knife spork node edit', "#{organization}#{current_user} edited node #{object_name}")
+      end
+
+      def after_nodedelete
+        send_message('knife spork node delete', "#{organization}#{current_user} deleted node #{object_name}")
+      end
+
+      def after_nodecreate
+        send_message('knife spork node create', "#{organization}#{current_user} created node #{object_name}")
+      end
+
+      def after_nodefromfile
+        send_message('knife spork node from file', "#{organization}#{current_user} uploaded node #{object_name}")
+      end
+
+      def after_noderunlistadd
+        send_message('knife spork node runlist add', "#{organization}#{current_user} added run_list items to #{object_name}: #{object_secondary_name}")
+      end
+
+      def after_noderunlistremove
+        send_message('knife spork node runlist remove', "#{organization}#{current_user} removed run_list items from #{object_name}: #{object_secondary_name}")
+      end
+
+      def after_noderunlistset
+        send_message('knife spork node runlist set', "#{organization}#{current_user} set the run_list for #{object_name} to #{object_secondary_name}")
+      end
+
       private
 
       def send_message(entity, message)
